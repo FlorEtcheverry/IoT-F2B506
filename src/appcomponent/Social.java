586cd8348d46352dc7f8e26f7d890e4493c228bf
FlorEtcheverry <florencia.a.etcheverry@gmail.com>
2017-02-27 16:00:38 +0100

    Social class implementing Twitter connection

---
+++ src/appcomponent/Social.java
@@ -0,0 +1,66 @@
+package appcomponent;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class Social {
+	
+	private String consumerKeyStr;
+	private String consumerSecretStr;
+	private String accessTokenStr;
+	private String accessTokenSecretStr;
+
+	public Social() {
+	}
+	
+	void setAuthValues(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
+		consumerKeyStr = consumerKey;
+		consumerSecretStr = consumerSecret;
+		accessTokenStr = accessToken;
+		accessTokenSecretStr = accessTokenSecret;
+	}
+	
+	boolean sendMedicationTookOnTime(String medName,int currentPoints) {
+		return sendTweet("I have taken my medication ("+medName+") within 30 minutes. Now I have "+currentPoints+" points.");
+	}
+
+	boolean sendMedicationTookOutOfTime(String medName, int currentPoints) {
+		return sendTweet("I have taken my medication ("+medName+") out of time (+/-30min). Now I have "+currentPoints+" points.");
+	}
+	
+	boolean sendMedicationTookDuplicated(String medName, int currentPoints) {
+		return sendTweet("I have taken my medication ("+medName+") duplicated. Now I have "+currentPoints+" points.");
+	}
+	
+	boolean sendMedicationNonTaken(String medName, int currentPoints) {
+		return sendTweet("I have not taken my medication ("+medName+"). Now I have "+currentPoints+" points.");
+	}
+
+	boolean sendMedicationFinished(String medName) {
+		return sendTweet("My medication ("+medName+") has finnished. Needs replacement.");
+	}
+	
+	private boolean sendTweet(String message) {
+		try {
+			Twitter twitter = new TwitterFactory().getInstance();
+
+			twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+			AccessToken accessToken = new AccessToken(accessTokenStr,
+					accessTokenSecretStr);
+
+			twitter.setOAuthAccessToken(accessToken);
+			twitter.updateStatus(message);
+			return true;
+
+		} catch (TwitterException te) {
+			if (te.getErrorCode()==186) {
+				sendTweet(message.substring(0, 140));
+			}
+			System.err.println("Couldn't send Twitter update. "+te.getMessage());
+			te.printStackTrace();
+			return false;
+		}
+	}
+}
